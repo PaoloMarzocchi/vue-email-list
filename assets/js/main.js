eebeb3c2d77e4d77424a8756aa146ed0437451a7
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-03-19 16:15:58 +0100

    add email list and print into DOM

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,22 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            
+            listMail: []
         }
     },
+    methods:{
+        getMailApi(){
+            for (let i = 0; i <= 9; i++) {
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+
+                    this.listMail.push(response.data.response)   
+            })
+        }
+        }
+    },
+    mounted(){
+        this.getMailApi();
+    }
 }).mount('#app')
